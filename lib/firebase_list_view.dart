aa8a6f4a263498ea11512dc38149f1e1a4ea8a07
xamfy <spratyaksh22@gmail.com>
2019-01-26 22:32:12 +0530

    Added InkWell to card

--- lib/firebase_list_view.dart
+++ lib/firebase_list_view.dart
@@ -24,39 +24,43 @@ class FirebaseListView extends StatelessWidget {
           Color iconColor = (status == true) ? Colors.greenAccent : Colors.grey;
           return Card(
             elevation: 8.0,
-            margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+            margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 8.0),
             color: c,
-            child: Container(
-              decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
-              child: ListTile(
-                contentPadding:
-                    EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-                title: Text(
-                  title,
-                  style: TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.bold),
-                ),
-                subtitle: Row(
-                  children: <Widget>[
-                    Container(
-                        padding: const EdgeInsets.all(6.0),
-                        decoration: new BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: iconColor,
-                        )),
-                    // Text("", style: TextStyle(color: Colors.white))
-                  ],
-                ),
-                trailing: Switch(
-                  activeColor: Colors.white,
-                  value: status,
-                  onChanged: (e) {
-                    status = !status;
-                    mainReference
-                        .child('devices')
-                        .child(index.toString())
-                        .set({"name": title, "status": status});
-                  },
+            child: InkWell(
+              onTap: () {},
+              child: Container(
+                decoration:
+                    BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
+                child: ListTile(
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                  title: Text(
+                    title,
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Row(
+                    children: <Widget>[
+                      Container(
+                          padding: const EdgeInsets.all(6.0),
+                          decoration: new BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: iconColor,
+                          )),
+                      // Text("", style: TextStyle(color: Colors.white))
+                    ],
+                  ),
+                  trailing: Switch(
+                    activeColor: Colors.white,
+                    value: status,
+                    onChanged: (e) {
+                      status = !status;
+                      mainReference
+                          .child('devices')
+                          .child(index.toString())
+                          .set({"name": title, "status": status});
+                    },
+                  ),
                 ),
               ),
             ),
