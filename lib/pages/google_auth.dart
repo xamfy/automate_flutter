caf2362cc3da88ff59d74d0ef5f54cfac8cacfc9
xamfy <spratyaksh22@gmail.com>
2019-02-10 19:02:37 +0530

    Added loading indicator while signing in

--- lib/pages/google_auth.dart
+++ lib/pages/google_auth.dart
@@ -19,6 +19,17 @@ class GoogleAuth extends StatefulWidget {
 class _GoogleAuthState extends State<GoogleAuth> {
   String _currentUser;
   SharedPreferences prefs;
+  bool _isLoading = false;
+
+  Widget _showCircularProgress() {
+    if (_isLoading) {
+      return Center(child: CircularProgressIndicator());
+    }
+    return Container(
+      height: 0.0,
+      width: 0.0,
+    );
+  }
 
   @override
   void initState() {
@@ -39,6 +50,9 @@ class _GoogleAuthState extends State<GoogleAuth> {
   }
 
   Future<FirebaseUser> _signIn(context) async {
+    setState(() {
+      _isLoading = true;
+    });
     prefs = await SharedPreferences.getInstance();
 
     var auth = AuthProvider.of(context).auth;
@@ -51,6 +65,9 @@ class _GoogleAuthState extends State<GoogleAuth> {
     String s = prefs.getString('photoUrl');
     // print("photoUrl (signIn):" + s);
 
+    setState(() {
+      _isLoading = false;
+    });
     mainReference
         .child('devices')
         .child(user.uid)
@@ -73,6 +90,91 @@ class _GoogleAuthState extends State<GoogleAuth> {
     }
   }
 
+  Widget _body(context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Text(
+            'Automate',
+            style: TextStyle(
+                fontSize: 30.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold),
+          ),
+          // SizedBox(height: 20.0),
+          new RaisedButton(
+            onPressed: () => _signIn(context).then((FirebaseUser user) {
+                  // Navigator.pushReplacement(
+                  //     context,
+                  //     MaterialPageRoute(
+                  //         builder: (BuildContext context) => HomePage(
+                  //               user: user,
+                  //               onSignedOut: () => _signOut(context),
+                  //             )));
+                  Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) => MyHomePage(
+                              // user: user,
+                              // onSignedOut: () => _signOut(context),
+                              )));
+                }).catchError((e) => print(e)),
+            color: Colors.white,
+            child: new Container(
+              width: 230.0,
+              height: 50.0,
+              alignment: Alignment.center,
+              child: new Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  new Padding(
+                    padding: const EdgeInsets.only(right: 20.0),
+                    // child: CachedNetworkImage(
+                    //   imageUrl:
+                    //       'https://developers.google.com/identity/images/g-logo.png',
+                    //   width: 30.0,
+                    // ),
+                    child: Image.asset(
+                      'assets/g-logo.png',
+                      height: 30.0,
+                    ),
+                  ),
+                  new Text(
+                    'Sign in With Google',
+                    textAlign: TextAlign.center,
+                    style: new TextStyle(
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          // RaisedButton(
+          //   child: Text('Log in with Google'),
+          //   onPressed: () => _signIn(context).then((FirebaseUser user) {
+          //         // Navigator.pushReplacement(
+          //         //     context,
+          //         //     MaterialPageRoute(
+          //         //         builder: (BuildContext context) => HomePage(
+          //         //               user: user,
+          //         //               onSignedOut: () => _signOut(context),
+          //         //             )));
+          //         Navigator.pushReplacement(
+          //             context,
+          //             MaterialPageRoute(
+          //                 builder: (BuildContext context) => MyHomePage(
+          //                     // user: user,
+          //                     // onSignedOut: () => _signOut(context),
+          //                     )));
+          //       }).catchError((e) => print(e)),
+          // ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     // print(_currentUser);
@@ -88,87 +190,11 @@ class _GoogleAuthState extends State<GoogleAuth> {
         //   title: Text('Automate'),
         //   automaticallyImplyLeading: false,
         // ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Text(
-                'Automate',
-                style: TextStyle(
-                    fontSize: 30.0,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold),
-              ),
-              // SizedBox(height: 20.0),
-              new RaisedButton(
-                onPressed: () => _signIn(context).then((FirebaseUser user) {
-                      // Navigator.pushReplacement(
-                      //     context,
-                      //     MaterialPageRoute(
-                      //         builder: (BuildContext context) => HomePage(
-                      //               user: user,
-                      //               onSignedOut: () => _signOut(context),
-                      //             )));
-                      Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                              builder: (BuildContext context) => MyHomePage(
-                                  // user: user,
-                                  // onSignedOut: () => _signOut(context),
-                                  )));
-                    }).catchError((e) => print(e)),
-                color: Colors.white,
-                child: new Container(
-                  width: 230.0,
-                  height: 50.0,
-                  alignment: Alignment.center,
-                  child: new Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      new Padding(
-                        padding: const EdgeInsets.only(right: 20.0),
-                        // child: CachedNetworkImage(
-                        //   imageUrl:
-                        //       'https://developers.google.com/identity/images/g-logo.png',
-                        //   width: 30.0,
-                        // ),
-                        child: Image.asset(
-                          'assets/g-logo.png',
-                          height: 30.0,
-                        ),
-                      ),
-                      new Text(
-                        'Sign in With Google',
-                        textAlign: TextAlign.center,
-                        style: new TextStyle(
-                          fontSize: 16.0,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              // RaisedButton(
-              //   child: Text('Log in with Google'),
-              //   onPressed: () => _signIn(context).then((FirebaseUser user) {
-              //         // Navigator.pushReplacement(
-              //         //     context,
-              //         //     MaterialPageRoute(
-              //         //         builder: (BuildContext context) => HomePage(
-              //         //               user: user,
-              //         //               onSignedOut: () => _signOut(context),
-              //         //             )));
-              //         Navigator.pushReplacement(
-              //             context,
-              //             MaterialPageRoute(
-              //                 builder: (BuildContext context) => MyHomePage(
-              //                     // user: user,
-              //                     // onSignedOut: () => _signOut(context),
-              //                     )));
-              //       }).catchError((e) => print(e)),
-              // ),
-            ],
-          ),
+        body: Stack(
+          children: <Widget>[
+            _body(context),
+            _showCircularProgress(),
+          ],
         ),
       );
     }
