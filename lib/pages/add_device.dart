98ef919a520ac876afa4df88e781efeef08f5374
xamfy <spratyaksh22@gmail.com>
2019-02-16 01:08:37 +0530

    Change add device UI

--- lib/pages/add_device.dart
+++ lib/pages/add_device.dart
@@ -10,7 +10,7 @@ class AddDevicePage extends StatelessWidget {
   final topAppBar = AppBar(
     elevation: 0.1,
     backgroundColor: Color.fromRGBO(58, 66, 86, 1.0),
-    title: Text('Add device'),
+    title: Text('Add Device'),
   );
   @override
   Widget build(BuildContext context) {
@@ -59,25 +59,56 @@ class _DeviceFormState extends State<DeviceForm> {
     final Size screenSize = MediaQuery.of(context).size;
 
     return Container(
-      padding: EdgeInsets.all(20.0),
+      padding: EdgeInsets.all(30.0),
       child: Form(
         key: this._formKey,
         child: ListView(
           children: <Widget>[
             TextFormField(
               textCapitalization: TextCapitalization.characters,
-              autofocus: true,
+              // autofocus: true,
               style: TextStyle(color: Colors.white, fontSize: 18.0),
               controller: deviceController,
               decoration: InputDecoration(
+                  filled: true,
+                  hintStyle: new TextStyle(color: Colors.grey[800]),
+                  // fillColor: Colors.white70
+                  // hintText: "Type in your text",
                   labelText: 'Device name',
+                  errorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                    color: Colors.white,
+                  )),
                   errorStyle: TextStyle(color: Colors.white),
-                  enabledBorder: new UnderlineInputBorder(
+                  border: new OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.white,
+                      width: 1.0,
+                      style: BorderStyle.solid,
+                    ),
+                    // borderRadius: const BorderRadius.all(
+                    //   const Radius.circular(10.0),
+                    // ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                      // borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                      borderSide: BorderSide(color: Colors.white)),
+
+                  // border: OutlineInputBorder(
+                  //   borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                  //   borderSide: BorderSide(
+                  //       color: Colors.white,
+                  //       width: 1.0,
+                  //       style: BorderStyle.solid),
+                  // ),
+                  enabledBorder: new OutlineInputBorder(
                     borderSide: BorderSide(
                         color: Colors.white,
                         width: 1.0,
-                        style: BorderStyle.none),
+                        style: BorderStyle.solid),
                   ),
+                  // isDense: true,
+                  // icon: Icon(Icons.add_photo_alternate, color: Colors.white),
                   // border: UnderlineInputBorder(
                   //   borderSide: BorderSide(color: Colors.white),
                   // ),
@@ -100,8 +131,11 @@ class _DeviceFormState extends State<DeviceForm> {
               child: RaisedButton(
                 padding: EdgeInsets.all(15.0),
                 child: Text(
-                  'Add device',
-                  style: TextStyle(color: Colors.black),
+                  'Add Device',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 17.0,
+                  ),
                 ),
                 onPressed: () {
                   // print(deviceController.text);
